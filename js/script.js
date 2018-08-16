576ab048d247bc1e964ad0c797f0f971882e824b
anyajunior <38322173+anyajunior@users.noreply.github.com>
2018-08-16 20:51:51 +0900

    copied the files from development branch
    dumped the unused ones. All trashy files including original template can be found in the dev branch

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function addValue(id) {
+  var numOfOrder = parseInt(document.getElementById(id).value);
+  numOfOrder++;
+  document.getElementById(id).value = numOfOrder.toString();
+}
+
+function minsValue(id) {
+  var numOfOrder = parseInt(document.getElementById(id).value);
+  if (numOfOrder <= 0) {
+    numOfOrder = 0;
+  } else {
+    numOfOrder--;
+  }
+  document.getElementById(id).value = numOfOrder.toString();
+}
+
+function getOrder() {
+  alert("Order added!");
+}
+
+
